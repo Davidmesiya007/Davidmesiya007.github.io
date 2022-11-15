1d4c7347475bc622c2dbea41cea1ef710a8be233
david <mddavid11204@gmail.com>
2022-11-15 23:30:24 +0530

    mobile nav and theme color developed

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -10,7 +10,7 @@ window.addEventListener("scroll", () => {
 });
 
 // theme colors
-const alternateStyles = document.querySelector(".alternate-style");
+const alternateStyles = document.querySelectorAll(".alternate-style");
 function setActiveStyle(color) {
   alternateStyles.forEach((style) => {
     if (color === style.getAttribute("title")) {
@@ -20,3 +20,18 @@ function setActiveStyle(color) {
     }
   });
 }
+
+// theme light and dark mode
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+  dayNight.querySelector("i").classList.toggle("fa-sun");
+  dayNight.querySelector("i").classList.toggle("fa-moon");
+  document.body.classList.toggle("dark");
+});
+window.addEventListener("load", () => {
+  if (document.body.classList.contains("dark")) {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  } else {
+    dayNight.querySelector("i").classList.add("fa-moon");
+  }
+});
